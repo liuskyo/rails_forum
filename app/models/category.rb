b1dc375d18b7af9aca31ae5509da659d0a6a22be
劉天佑 <gn00595067@gmail.com>
2015-12-23 10:32:41 +0800

    no message

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
 	has_many :category_essayships
+	has_many :essays,:through=>:category_essayships
 end
--- app/models/essay.rb
+++ app/models/essay.rb
@@ -2,4 +2,5 @@ class Essay < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	has_many :category_essayships
+	has_many :categorys,:through=>:category_essayships
 end
